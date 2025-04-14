69590be3012140584430a3b89448bebac3f14051
SingDogYww <2281435866@qq.com>
2025-04-14 19:59:57 +0800

    新建项目

---
+++ question-bank-system-backend/src/main/java/cn/cxyxh/project/question/QuestionBankSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.cxyxh.project.question;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionBankSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionBankSystemBackendApplication.class, args);
+    }
+
+}
---
+++ question-bank-system-backend/src/test/java/cn/cxyxh/project/question/QuestionBankSystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.cxyxh.project.question;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionBankSystemBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
